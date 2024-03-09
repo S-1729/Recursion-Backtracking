107c56a12d198cfe2177fff16ca60d680b57ab72
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-03-09 21:31:01 +0530

    Add files via upload

---
+++ Pow(x, n).cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double Pow(double x, int n) {
+        if(n==0)
+            return 1;
+        double ans=Pow(x,n/2);
+        if(n&1)
+            return x*ans*ans;
+        else
+            return ans*ans;
+    }
+    
+    double myPow(double x, int n) {
+        if(n>=0)
+            return Pow(x,n);
+        return 1/Pow(x,n);
+    }
+};
+
+/*
+	Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+	
+	Example 1:
+	Input: x = 2.00000, n = 10
+	Output: 1024.00000
+	
+	Example 2:
+	Input: x = 2.10000, n = 3
+	Output: 9.26100
+	
+	Example 3:
+	Input: x = 2.00000, n = -2
+	Output: 0.25000
+	Explanation: 2-2 = 1/22 = 1/4 = 0.25
+	
+	T.C : O(logn)
+*/
