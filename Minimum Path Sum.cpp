cdab6724ebae9179c0124953419660376ca94876
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-03-18 19:47:54 +0530

    Add files via upload

--- Minimum Path Sum.cpp
+++ Minimum Path Sum.cpp
@@ -16,6 +16,23 @@ public:
     }
 };
 
+//Code-2
+class Solution {
+public:
+    int solve(int i,int j,int n,int m,vector<vector<int>>& grid){
+        if(i==n-1 && j==m-1)
+            return grid[i][j];
+        if(i>=n || j>=m)
+            return INT_MAX;
+        return grid[i][j]+min(solve(i+1,j,n,m,grid),solve(i,j+1,n,m,grid));
+    }
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        return solve(0,0,n,m,grid);
+    }
+};
+
 /*
 	Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
 	Note: You can only move either down or right at any point in time.
